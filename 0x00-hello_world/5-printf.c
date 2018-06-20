5a938b3a8a4c6573ea1fccb5b0dac9c76ca50675
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-06-20 22:53:13 +0000

    use Betty style and added documentation

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
-int main()
+/**
+ * main - use printf to output text
+ * Return: 0
+ */
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-
-int main() 
+/**
+ * main - use printf to display sizes
+ * Return: 0
+ */
+int main(void) 
 {
 	printf ("Size of a char: %d byte(s)\n", sizeof(char));
 	printf ("Size of a int: %d byte(s)\n", sizeof(int));

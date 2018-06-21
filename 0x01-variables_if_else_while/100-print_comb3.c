53bfd865f3f5d5a9a6e85eb64fef5db0612bd6b9
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-06-21 22:20:52 +0000

    bonus 1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - print 0 through 9 using putchar without using char
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int offset;
+
+	offset = 0;
+	for (i = 48; i < 58; i++)
+	{
+		j = i + offset;
+		for (; j < 58; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != 56 || j != 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+		offset = 1;;
+	}
+	putchar('\n');
+	return (0);
+}

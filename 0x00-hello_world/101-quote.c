beb560b500a224ec50c61b96046cba4e5470fa4a
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-06-21 04:12:19 +0000

    bonus

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,12 @@
  */
 int main(void)
 {
-	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char str1[] = "and that piece of art is useful";
+	char str3[] = "\"";
+	char str2[] = " - Dora Korpar, 2015-10-19\n";
 
 	write(2, str1, strlen(str1));
+	write(0, str3, 1);
+	write(2, str2, strlen(str2));
 	return (1);
 }

e9195f61f5db202d53a0c9524b152995d6d93359
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-06-21 01:37:04 +0000

    solutions

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 /**
  * main -  write to stderr
  * Return: 1
  */
 int main(void)
 {
-	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n;
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, str1, strlen(str1));
 	return (1);

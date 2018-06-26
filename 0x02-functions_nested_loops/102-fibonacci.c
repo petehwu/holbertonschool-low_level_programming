4b8a9b3583d0d1b0199899e95c62f7e4c54526a0
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-06-26 18:44:14 +0000

    solution for bonus

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - print first 50 fibonacci numbers
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+	long int y;
+	long int z;
+	long int tot;
+
+	y = 1;
+	z = 2;
+	printf("1, 2, ");	
+	for (i = 0; i < 48; i++)
+	{
+		tot = y + z;
+		if ( i == 47)
+			printf("%ld", tot);
+		else
+			printf("%ld, ", tot);
+		y = z;
+		z = tot;
+	}
+	printf("\n");
+	return (0);
+}

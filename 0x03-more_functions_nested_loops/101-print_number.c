d698baeda34cf458d0a0b3e2b4991ad7a6806b5b
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-06-28 21:54:07 +0000

    bonus

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -7,18 +7,17 @@
 
 void print_number(int n)
 {
-	int x = 10;
 	int t;
 
 	t = n;
-	if (t < 0 && t / x == 0)
-		_putchar('-');
-	t /= x;
+	if (t < 0 && t / 10 == 0)
+		_putchar(45);
+	t /= 10;
 	if (t != 0)
 		print_number(t);
 
 	if (n < 0)
-		_putchar('0' + (n % x) * -1);
+		_putchar('0' + (n % 10) * -1);
 	else
-		_putchar('0' + (n % x));
+		_putchar('0' + (n % 10));
 }

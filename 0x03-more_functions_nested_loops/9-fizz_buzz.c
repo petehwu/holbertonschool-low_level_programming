645ba078b63250f99972cb63e47961a4ffccf340
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-06-28 21:17:58 +0000

    removed space character after last number

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,16 +16,18 @@ int main(void)
 		if (mod3 == 0 || mod5 == 0)
 		{
 			if (mod3 == 0 && mod5 == 0)
-				printf("FizzBuzz ");
+				printf("FizzBuzz");
 			else if (mod3 == 0)
-				printf("Fizz ");
+				printf("Fizz");
 			else
-				printf("Buzz ");
+				printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", x);
+			printf("%d", x);
 		}
+		if (x < 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);

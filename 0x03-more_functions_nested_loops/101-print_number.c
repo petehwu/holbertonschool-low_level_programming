60a5d029b44df870cc48880e2c2f24bfd086a8c5
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-06-29 04:47:26 +0000

    non-recursive solution

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -7,12 +7,46 @@
 
 void print_number(int n)
 {
-	if (n < 0 && n / 10 == 0)
-		_putchar('-');
-	if (n / 10 != 0)
-		print_number(n / 10);
+	int i = 0, j = 10, x, k;
+
+	x = n;
 	if (n < 0)
-		_putchar('0' + ((n % 10) * -1));
+	_putchar('-');
+	if (n > -10 && n < 10)
+	{
+		if (n < 0)
+			_putchar('0' + (n % 10) * -1);
+		else
+			_putchar('0' + n % 10);
+	}
 	else
-		_putchar('0' + (n % 10));
+	{
+		while (x / 10 >= 10 || x / 10 <= -10)
+		{
+			x /= 10;
+			j *= 10;
+			i++;
+		}
+		for (k = 0; k < i; k++)
+		{
+			if (n < 0)
+				_putchar('0' + (n / j) * -1);
+			else
+				_putchar('0' + n / j);
+			n %= j;
+			j /= 10;
+		}
+		if (n < 0)
+		{
+			_putchar('0' + (n / 10) * -1);
+			_putchar('0' + (n % 10) * -1);
+		}
+		else
+		{
+			_putchar('0' + n / 10);
+			_putchar('0' + n % 10);
+		}
+	}
+
+
 }

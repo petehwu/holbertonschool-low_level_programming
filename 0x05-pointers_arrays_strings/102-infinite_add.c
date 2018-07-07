8e21b4c91e85a5b8915330955f60b669669a9939
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-07-07 22:08:48 +0000

    initialized all variables

--- 0x05-pointers_arrays_strings/102-infinite_add.c
+++ 0x05-pointers_arrays_strings/102-infinite_add.c
@@ -5,7 +5,7 @@
 void rev_string(char *n)
 {
 	int i = 0, j = 0;
-	char temp;
+	char temp = '';
 
 	while (*(n + i) != '\0')
 		i++;
@@ -29,7 +29,7 @@ void rev_string(char *n)
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int overflow = 0, i = 0, j = 0, digits = 0;
-	int val1, val2, temp_tot = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
 
 	while (*(n1 + i) != '\0')
 		i++;

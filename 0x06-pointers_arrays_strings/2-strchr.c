576ad6196acd8c3bb7c87a8ff0dca23c0e9c70de
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-07-10 17:11:33 +0000

    initial commit #2

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+/**
+ * _strchr - locates a character in string
+ * @s: string to search
+ * @c: character to look for
+ * Return: pointer to first occurence or Null if not found
+ */
+
+char *_strchr(char *s, char c)
+{
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+			break;
+		s++;
+	}
+	if (*s == '\0')
+		return ('\0');
+	else
+		return (s);
+}

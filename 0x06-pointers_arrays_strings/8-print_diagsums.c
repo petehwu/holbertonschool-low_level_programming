1d6b86c96a1a3609fcf00f0c82bebaca61c22bd6
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-07-11 02:25:48 +0000

    initial commit for 8

---
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * print_diagsums - print sum of diagonals in square matrix
+ * @a: pointer to array
+ * @size: size of array
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int row, sum1 = 0, sum2 = 0;;
+
+	for (row = 0; row < size; row++)
+	{
+		sum1 = sum1 + a[(row * size) + row];
+		sum2 = sum2 + a[(row + 1) * size - row - 1];
+	}
+	printf("%i, %i\n", sum1, sum2);
+}

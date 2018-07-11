784ad021fbb76760666725c95c9fe02fb8f29bd6
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-07-11 22:17:59 +0000

    initial commit for 7

--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 /**
  * str_len - returns length of string
  * @s: string to count
@@ -9,27 +8,45 @@ int str_len(char *s)
 	int len = 0;
 
 	if (*s)
-		len = 1 + str_len(s+1);
+		len = 1 + str_len(s + 1);
 	return (len);
+}
+/**
+ * add_char - add chars
+ * @s: pointer to string
+ * @sw: switch position
+ * Return: sum of the string
+ */
+int add_char(char *s, int sw)
+{
+	int len = 0, tot = 0;
+
+	len = str_len(s);
+	if (*s)
+		tot = add_char(s + 1, sw);
+	if (len - sw > 0)
+		tot -= *s;
+	if (len - sw  <= 0)
+		tot += *s;
+	return (tot);
 }
 
 /**
  * is_palindrome - determines if palindrome
  * @s: string to check
+ * Return: 1 if palindrome else 0
  */
 int is_palindrome(char *s)
 {
-	int len = 0, tot = 0;
+	int len = 0, sw, final;
 
 	len = str_len(s);
-	printf("len: %i \n", len);
-	if (*s)
-		tot = is_palindrome(s+1);
-	if (len / 2 >= 2)
-		tot -= *s;
-	if (len / 2 < 2)
-		tot += *s;
-	printf("tot: %i ", tot);
-	return (tot);
-			
+	sw = len / 2;
+	final = add_char(s, sw);
+	if (len % 2 != 0)
+		final += s[len / 2];
+	if (final > 0)
+		return (0);
+	else
+		return (1);
 }

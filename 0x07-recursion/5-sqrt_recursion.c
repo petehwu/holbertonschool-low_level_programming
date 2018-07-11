bd2eeafda68ee89828fdfd8f8d303d6c1e5bcdd3
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-07-11 22:43:07 +0000

    initial solution for 5

---
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+/**
+ * do_calc - perform recursive multiply
+ * @n: value to find sqrt for
+ * @c: counter
+ * Return: -1 or sqrt value
+ */
+int do_calc(int n, int c)
+{
+	int found = 0;
+
+	if (c  < n / 2)
+		found = do_calc(n, c + 1);
+	if (c * c == n)
+		return (c);
+	else if (found > 0)
+		return (found);
+	else
+		return (-1);
+}
+/**
+ * _sqrt_recursion - finds sqare root of number
+ * @n: number to find square root for
+ * Return: Square root of n or -1 if no natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	return (do_calc(n, 0));
+}

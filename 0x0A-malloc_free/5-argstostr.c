a6031ecc33738cb71754b0f3c9f76fd2bde44481
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-07-17 22:28:32 +0000

    initial solution for 5

---
+++ 0x0A-malloc_free/5-argstostr.c
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+/**
+ * _strlen - returns length of string
+ * @s: string to find length
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	if (*s)
+		return (_strlen(s + 1) + 1);
+	else
+		return (0);
+}
+/**
+ * tot_len - count total lengths of all args
+ * @ac: num of arguments
+ * @av: pointer to first arg
+ * Return: total bytes
+ */
+int tot_len(int ac, char **av)
+{
+	int tot = 0;
+
+	while (ac--)
+	{
+		tot += _strlen(*(av + ac)) + 1;
+	}
+	return (tot);
+}
+/**
+ * argstostr - concatenates all arguments
+ * @ac: number of parameters
+ * @av: pointer to first parameter
+ * Return: pointer to new string
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *ns, *temp;
+	int i, len, index = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	len = tot_len(ac, av);
+	ns = malloc(sizeof(char) * len);
+	if (!ns)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		temp = *(av + i);
+		while (*temp)
+		{
+			*(ns + index) = *temp;
+			index++;
+			temp++;
+		}
+		*(ns + index) = '\n';
+		index++;
+	}
+	return (ns);
+}

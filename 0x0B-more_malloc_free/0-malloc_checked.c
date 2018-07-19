f57ee568b0b882b052705e286667f6fcd45b9b5c
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-07-19 14:56:52 +0000

    fixed betty issue

--- 0x0B-more_malloc_free/0-malloc_checked.c
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 /**
- * malloc_checked - allocates memory 
+ * malloc_checked - allocates memory
  * @b: size of memory to allocate
  * Return: pointer to allocated memory
  */
@@ -10,7 +10,7 @@ void *malloc_checked(unsigned int b)
 
 	p = malloc(b);
 	if (!p)
-		exit (98);
+		exit(98);
 	else
 		return (p);
 }

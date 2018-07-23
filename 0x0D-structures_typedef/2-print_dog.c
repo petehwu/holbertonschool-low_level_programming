d5ff36b115f37f66eb41c9b4cf3ba41d9d9201a3
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-07-23 20:28:48 +0000

    removed age null check because numbers cannot be null

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -9,7 +9,7 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		printf("Name: %s\n", d->name ? d->name : "(nil)");
-		d->age ? printf("Age: %f\n", d->age) : printf("Age: (nil)\n");
+		printf("Age: %f\n", d->age);
 		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 	}
 }

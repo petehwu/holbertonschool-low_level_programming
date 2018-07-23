c6be0cfb94101a1aca42b12788a63f67a605a87e
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-07-23 20:24:50 +0000

    changed age print from %.1f to %f

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -9,7 +9,7 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		printf("Name: %s\n", d->name ? d->name : "(nil)");
-		d->age ? printf("Age: %.1f\n", d->age) : printf("Age: (nil)\n");
+		d->age ? printf("Age: %f\n", d->age) : printf("Age: (nil)\n");
 		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 	}
 }

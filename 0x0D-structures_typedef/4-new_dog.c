20a3ad2575c813f39599e7c3691472e65f514af0
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-07-23 22:05:06 +0000

    changed the code for making copy of owner and name

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -9,13 +9,19 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	char __attribute__((unused)) *cname; 
+	char __attribute__((unused)) *cowner;
 	dog_t *doggy;
 
 	doggy = malloc(sizeof(dog_t));
+	cname = malloc(sizeof(char *));
+	cowner = malloc(sizeof(char *));
 	if (!doggy)
 		return (NULL);
 	doggy->name = name;
 	doggy->age = age;
 	doggy->owner = owner;
+	cname = doggy->name;
+	cowner = doggy->owner;
 	return (doggy);
 }

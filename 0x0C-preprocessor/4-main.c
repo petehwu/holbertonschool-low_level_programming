24a97b81d647cdb081a31faef2fcf0f8d2f2684c
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-07-23 16:29:36 +0000

    solution for 4

---
+++ 0x0C-preprocessor/4-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "4-sum.h"
+#include "4-sum.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int s,t;
+
+    s = SUM(98, 1024);
+    t = SUM(-1 * -5, -5);
+    printf("%d, %d\n", s, t);
+    return (0);
+}
---
+++ 0x0C-preprocessor/4-sum.h
@@ -0,0 +1,4 @@
+#ifndef __CALC_SUM_H
+#define __CALC_SUM_H
+#define SUM(X, Y) ((X) + (Y))
+#endif

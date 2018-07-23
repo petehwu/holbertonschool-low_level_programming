7b9fafd740e39734c337ddadd3941e74afc7b4c6
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-07-23 22:18:04 +0000

    added helper function to make copies of variables again

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,6 +1,30 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
+ * make_copy - copies a variable
+ * @p: pointer to variable to be copied
+ * Return: pointer to new copy 
+ */
+char *make_copy(char *p)
+{
+	int i;
+	char *np;
+
+	if (!p)
+		return (NULL);
+	while (*(p + i))
+		i++;
+	i++;
+	np = malloc(i);
+	i--;
+	while (i >= 0)
+	{
+		*(np + i) = *(p + i);
+		i--;
+	}
+	return (np);
+}
+/**
  * new_dog - instantiate a new dog object
  * @name: name of dog
  * @age: age of dog
@@ -9,7 +33,7 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char __attribute__((unused)) *cname; 
+	char __attribute__((unused)) *cname;
 	char __attribute__((unused)) *cowner;
 	dog_t *doggy;
 
@@ -21,7 +45,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	doggy->name = name;
 	doggy->age = age;
 	doggy->owner = owner;
-	cname = doggy->name;
-	cowner = doggy->owner;
+	if (!name)
+		cname = make_copy(name);
+	if (!owner)
+		cowner = make_copy(owner);
 	return (doggy);
 }

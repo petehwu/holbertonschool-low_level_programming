fbf709fcb4edcca25e96ae0e6600bad69692d056
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-07-23 18:42:57 +0000

    solution for 0

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef __DOG_H
+#define __DOG_H
+/**
+ * struct dog - is a dog
+ * @name: pointer to name of dog
+ * @age: age of dog
+ * @owner: pointer to name of owner
+ *
+ * Description: struct for a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

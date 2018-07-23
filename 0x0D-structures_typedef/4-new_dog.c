b7f3c2ceab551f51fa076e5fc3677110363e0012
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-07-23 21:52:52 +0000

    removed the code that makes copies of owner and name

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,33 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- * make_copy - make a copy of variable
- * @p: pointer to variable to be copied
- * Return: pointer to copied variable
- */
-char *make_copy(char *p)
-{
-	int i = 0;
-	char *np;
-
-	if (!p)
-		return (NULL);
-	while (*(p + i))
-		i++;
-	i++;
-	np = malloc(i);
-	if (!np)
-		return (NULL);
-	i--;
-	while (i >= 0)
-	{
-		*(np + i) = *(p + i);
-		i--;
-	}
-	return (np);
-}
-
-/**
  * new_dog - instantiate a new dog object
  * @name: name of dog
  * @age: age of dog
@@ -36,19 +9,13 @@ char *make_copy(char *p)
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char __attribute__((unused)) *cname;
-	char __attribute__((unused)) *cowner;
 	dog_t *doggy;
 
 	doggy = malloc(sizeof(dog_t));
-	cname = make_copy(name);
-	cowner = make_copy(owner);
 	if (!doggy)
 		return (NULL);
-	if (name)
-		doggy->name = name;
+	doggy->name = name;
 	doggy->age = age;
-	if (owner)
-		doggy->owner = owner;
+	doggy->owner = owner;
 	return (doggy);
 }

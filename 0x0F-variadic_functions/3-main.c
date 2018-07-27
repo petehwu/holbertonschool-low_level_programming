c7cf94c2592cb68d0a288c09f00ebd0de8bd8435
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-07-27 16:44:44 -0700

    solution for 3

--- 0x0F-variadic_functions/3-main.c
+++ 0x0F-variadic_functions/3-main.c
@@ -8,5 +8,11 @@
 int main(void)
 {
     print_all("ceis", 'H', 0, "lberton");
+    print_all("ceiss", 'H', 0, 0, "good?");
+    print_all("xxxxxceisssxxx", 'H', 0, 0, "good?", 0);
+    print_all("xx");
+    print_all("sx", "");
+    print_all(0);
+    print_all(0, 'A', 0, 0, "YO");
     return (0);
 }
--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -8,30 +8,52 @@ void print_all(const char * const format, ...)
 {
 	va_list parmlist;
 	int i = 0;
+	int printflag = 0;
 	char cn;
+	char *sp;
 
-	va_start(parmlist, format);
-	while (*(format + i))
+	if (format)
 	{
-		switch (*(format + i))
+		va_start(parmlist, format);
+		while (*(format + i))
 		{
-			case 'c':
-				printf("%c", (char) va_arg(parmlist, int));
-				break;
-			case 'i':
-				printf("%i", va_arg(parmlist, int));
-				break;
-			case 'f':
-				printf("%f", (float) va_arg(parmlist, double));
-				break;
-			case 's':
-				printf("%s", va_arg(parmlist, char *));
+			switch (*(format + i))
+			{
+				case 'c':
+					printf("%c", (char) va_arg(parmlist, int));
+					printflag = 1;
+					break;
+				case 'i':
+					printf("%i", va_arg(parmlist, int));
+					printflag = 1;
+					break;
+				case 'f':
+					printf("%f", (float) va_arg(parmlist, double));
+					printflag = 1;
+					break;
+				case 's':
+					sp = va_arg(parmlist, char *);
+					printflag = 1;
+					switch ((int) !sp)
+					{
+						case 0:
+							printf("%s", sp);
+							break;
+						default:
+							printf("(nil)");
+					}
+			}
+			cn = *(format + i + 1);
+			if (cn &&
+					printflag == 1 &&
+					(cn == 'c' || cn == 'i' || cn == 'f' || cn == 's'))
+			{
+				printf(", ");
+				printflag = 0;
+			}
+			i++;
 		}
-		cn = *(format + i + 1);
-		if (cn && (cn == 'c' || cn == 'i' || cn == 'f' || cn == 's'))
-			printf(", ");
-		i++;
+		va_end(parmlist);
 	}
-	va_end(parmlist);
 	printf("\n");
 }

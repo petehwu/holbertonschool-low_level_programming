f0dcc4dccef4a6c2f72b3854238d3dfa55da42bb
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-07-27 16:56:33 -0700

    added an alternate solution using 2 while and 2 if and no weird typecasting for switch

---
+++ 0x0F-variadic_functions/3-alternate-print_all.c
@@ -0,0 +1,55 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments passed in
+ */
+void print_all(const char * const format, ...)
+{
+	va_list parmlist;
+	int i = 0;
+	int printflag = 0;
+	char cn;
+	char *sp;
+
+	while (format)
+	{
+		va_start(parmlist, format);
+		while (*(format + i))
+		{
+			switch (*(format + i))
+			{
+				case 'c':
+					printf("%c", (char) va_arg(parmlist, int));
+					printflag = 1;
+					break;
+				case 'i':
+					printf("%i", va_arg(parmlist, int));
+					printflag = 1;
+					break;
+				case 'f':
+					printf("%f", (float) va_arg(parmlist, double));
+					printflag = 1;
+					break;
+				case 's':
+					sp = va_arg(parmlist, char *);
+					printflag = 1;
+					if (!sp)
+						sp = "(nil)";
+					printf("%s", sp);
+			}
+			cn = *(format + i + 1);
+			if (cn &&
+					printflag == 1 &&
+					(cn == 'c' || cn == 'i' || cn == 'f' || cn == 's'))
+			{
+				printf(", ");
+				printflag = 0;
+			}
+			i++;
+		}
+		va_end(parmlist);
+		break;
+	}
+	printf("\n");
+}

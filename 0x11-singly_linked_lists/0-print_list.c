f6ba935e71d5798ff5f1350916b9f247105d171e
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-08-02 10:01:48 -0700

    using length from struct instead of counting with loop

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -1,4 +1,4 @@
-#include <stdioh>
+#include <stdio.h>
 #include "lists.h"
 /**
  * print_list - prints all elements of a list
@@ -7,24 +7,16 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t str_cnt = 0, ele_cnt = 0;
+	size_t  ele_cnt = 0;
 
 	if (!h)
 		return (0);
 	while (h)
 	{
 		if (!(h->str))
-		{
 			printf("[0] (nil)");
-		}
 		else
-		{
-			str_cnt = 0;
-			while (h->str[str_cnt])
-				str_cnt++;
-			printf("[%zd] %s", str_cnt, h->str);
-
-		}
+			printf("[%u] %s", h->len, h->str);
 		printf("\n");
 		h = h->next;
 		ele_cnt++;

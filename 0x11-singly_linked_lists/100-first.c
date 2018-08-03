7bb201cc3a46d7e20a92a7d4d464190bd940e551
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-08-02 23:08:17 -0700

    fixed betty issues

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-void startup (void) __attribute__ ((constructor));
+void startup(void) __attribute__ ((constructor));
 
 /**
  * startup - function to be called before main
  */
-void startup (void)
+void startup(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }

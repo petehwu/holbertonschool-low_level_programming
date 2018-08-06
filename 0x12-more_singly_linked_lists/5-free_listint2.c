8dc3599a576bbbab9dcfac1a79038bf44510f677
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-08-06 13:20:05 -0700

    solution for 5

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees memory for list by setting head to null
+ * @head: pointer to head of list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp_node;
+
+	if (*head)
+	{
+		tmp_node = (*head)->next;
+		while (*head)
+		{
+			free(*head);
+			*head = tmp_node;
+			if (*head)
+				tmp_node = (*head)->next;
+		}
+	}
+}

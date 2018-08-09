eda67aa658b3ebe29071fe74eadfa7e1174c363b
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-08-09 11:04:28 -0700

    solution for 2

---
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * get_bit - returns bit value at given index
+ * @n: number
+ * @index: index position to get
+ * Return : bit value at given position
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int digits = 0, i = 0;
+	unsigned long int temp;
+
+	temp = n;
+	while (temp)
+	{
+		temp = temp >> 1;
+		digits++;
+	}
+	digits--;
+	temp = n;
+	if (index > digits)
+		return (-1);
+	for (i = 0 ; i < index; i++)
+		temp = temp >> 1;
+	return (temp & 1);
+
+
+
+
+}

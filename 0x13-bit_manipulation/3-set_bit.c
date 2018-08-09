9414b2c6c0ec453f3152415b161329852056b88c
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-08-09 11:48:43 -0700

    solution for 3

---
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * set_bit - sets bit value to 1 at given index
+ * @n: number
+ * @index: index position to set
+ * Return: 1 if successful -1 if not
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int digits = 0, i = 0;
+	unsigned long int temp;
+
+	temp = *n;
+	while (temp)
+	{
+		temp = temp >> 1;
+		digits++;
+	}
+	digits--;
+	temp = 1;
+	if (index > sizeof(long int) * 8)
+		return (-1);
+	for (i = 0; i < index; i++)
+		temp = temp << 1;
+	*n = *n | temp;
+	return (*n);
+}

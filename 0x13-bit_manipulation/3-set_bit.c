43ca67c23e46b8eaa9ff6a731f835005e3cc5b23
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-08-09 22:39:35 -0700

    refactored code to remove extra un-necessary loops and processing

--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -7,20 +7,11 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int digits = 0;
 	unsigned long int temp;
 
-	temp = *n;
-	while (temp)
-	{
-		temp = temp >> 1;
-		digits++;
-	}
-	digits--;
-	temp = 1;
 	if (index > sizeof(long int) * 8)
 		return (-1);
-	temp = temp << index;
+	temp = 1 << index;
 	*n = *n | temp;
 	return (1);
 }

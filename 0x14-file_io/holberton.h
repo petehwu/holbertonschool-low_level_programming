4bbf0480161c360133998edeeca6303f46230390
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-08-14 13:48:31 -0700

    update header file for question 3

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -3,11 +3,13 @@
 
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#define BUFF_SIZE 1024
 /**
  * read_textfile - reads a text file and prints it
  * @filename: name of file to read

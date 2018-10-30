8fdd8e0f71201badc5d1a10e0e112ba9065c8ba1
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-10-30 10:03:19 -0700

    changed logic for insertion sort

--- 0x1A-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -16,19 +16,32 @@ void insertion_sort_list(listint_t **list)
 		nextnode = curnode->next;
 		if (nextnode && curnode->n > nextnode->n)
 		{
-			curnode->next = nextnode->next;
-			nextnode->prev = curnode->prev;
-			nextnode->next = curnode;
-			curnode->prev = nextnode;
-			if (nextnode->prev)
-				nextnode->prev->next = nextnode;
-			else
-				*list = nextnode;
-			if (curnode->next)
-				curnode->next->prev = curnode;
-			print_list(*list);
-			nextnode = nextnode->prev ? nextnode->prev : *list;
+			insertion_sort_swapper(list, curnode, nextnode);
+			while (nextnode->prev && nextnode->prev->n > nextnode->n)
+				insertion_sort_swapper(list,  nextnode->prev, nextnode);
+			nextnode = curnode;
 		}
 		curnode = nextnode;
 	}
+}
+
+/**
+ * insertion_sort_swapper - swaps two nodes in a linked list
+ * @list: pointer to pointer of linked list
+ * @from: pointer to swap from
+ * @to: pointer to swap to
+ */
+void insertion_sort_swapper(listint_t **list, listint_t *from, listint_t *to)
+{
+	to->prev = from->prev;
+	from->next = to->next;
+	to->next = from;
+	from->prev = to;
+	if (from->next)
+		from->next->prev = from;
+	if (to->prev)
+		to->prev->next = to;
+	else
+		*list = to;
+	print_list(*list);
 }
